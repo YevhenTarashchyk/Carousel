eda6000040c4f3b8a31968ef706a0cd7acb6baa9
Yevhen_Tarashchyk <maze.ultramarine@gmail.com>
2019-10-02 15:58:44 +0300

    initialCommit
    initial commit

---
+++ build/precache-manifest.73f83e1cbf66f3f64da2def141fdeaa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adadaa2bdeda77bc4cac1ef83ce19141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c944967d5cb41ad6c744",
+    "url": "/static/css/main.6325aa8a.chunk.css"
+  },
+  {
+    "revision": "3e2ff6b31e0cebdb3456",
+    "url": "/static/js/2.3f4c7305.chunk.js"
+  },
+  {
+    "revision": "c944967d5cb41ad6c744",
+    "url": "/static/js/main.50b3bdbd.chunk.js"
+  },
+  {
+    "revision": "88c286c11f557557c5f1",
+    "url": "/static/js/runtime-main.14d3986b.js"
+  }
+]);
